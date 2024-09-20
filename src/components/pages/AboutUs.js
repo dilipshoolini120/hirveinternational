c1c15b54506819203427505290c9511d3239fff9
dilip <dilipmaurya@gmail.com>
2024-09-20 10:17:02 +0530

    error to be fixed!

--- src/components/pages/AboutUs.js
+++ src/components/pages/AboutUs.js
@@ -1,11 +1,14 @@
 import React from "react";
 import '../../App.css';
 import Footer from "../Footer";
+import Element from "../Element";
+
 
 export default function AboutUs() {
     return(
     <>
-    <h1 className="about-us">ABOUT US</h1>
+    {/* <h1 className="about-us">HIRVE INTERNATIONAL</h1> */}
+    <Element/>
     <Footer/>
     </>
     )
